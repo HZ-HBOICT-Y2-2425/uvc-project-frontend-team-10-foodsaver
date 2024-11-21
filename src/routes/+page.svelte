693da42b980901295422e32fb3e99ca3de026456
Luyue28 <zhan0067@hz.nl>
2024-11-21 17:05:53 +0100

    using route instead of a component to display recipes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,21 @@
 <script>
-  import RecipeSearch from '../components/RecipeSearch.svelte';
-  import Pantry from '../components/Pantry.svelte';
-  
+  import RecipeSearch from "../components/RecipeSearch.svelte";
+  import Pantry from "../components/Pantry.svelte";
+  import { goto } from "$app/navigation";
+
+  function navigateToRecipe() {
+    goto("/recipe");
+  }
 </script>
+
+<div>
+  <button
+    on:click={navigateToRecipe}
+    class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 mt-4"
+  >
+    Go to Recipe Page
+  </button>
+</div>
 
 <div class="main-content mt-4 flex-1 flex">
   <RecipeSearch />

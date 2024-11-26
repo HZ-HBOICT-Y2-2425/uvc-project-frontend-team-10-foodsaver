be79873bd270e858e3aff7a5995fd21ad074e079
100bomby <vyna0001@hz.nl>
2024-11-26 15:58:29 +0100

    ESLint working

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,9 @@
-// eslint.config.js
-export default {
-    extends: [
-      'eslint:recommended', // Use the recommended ESLint rules
-      'plugin:svelte/recommended' // Add Svelte-specific linting if using Svelte
-    ],
-    parserOptions: {
-      ecmaVersion: 'latest', // Use the latest JavaScript version
-      sourceType: 'module' // Allow ES6 modules
-    },
-    env: {
-      browser: true, // If you are targeting a browser environment
-      node: true // If you are using Node.js
-    },
-  };
-  
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+];

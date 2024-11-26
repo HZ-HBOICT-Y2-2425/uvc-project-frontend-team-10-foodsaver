7cf87b03fa4a38e245509d5d3a24c097b964dc1a
100bomby <vyna0001@hz.nl>
2024-11-26 16:10:45 +0100

    Should be working

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,11 @@ import pluginJs from "@eslint/js";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
+    {
+        ignores: [
+          '**/.svelte-kit/**',  // Exclude all files in the .svelte-kit directory
+        ],
+      },
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
 ];

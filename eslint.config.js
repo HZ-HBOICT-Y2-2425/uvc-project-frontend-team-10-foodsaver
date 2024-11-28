36aefe5e03e166791d8d160edf6060be96f34c69
100bomby <vyna0001@hz.nl>
2024-11-28 11:44:25 +0100

    ESlint added

--- eslint.config.js
+++ eslint.config.js
@@ -4,11 +4,9 @@ import pluginJs from "@eslint/js";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-    {
-        ignores: [
-          '**/.svelte-kit/**',  // Exclude all files in the .svelte-kit directory
-        ],
-      },
   {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,n
+  pluginJs.configs.recommended,
+  {
+    ignores: ["node_modules", ".svelte-kit/"],
+  }
 ];
